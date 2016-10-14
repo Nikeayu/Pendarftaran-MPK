ba9bdcd65d087047febdfd15036c6206ed50ff51
ELLEN <nikeayuv15@gmail.com>
2016-10-14 22:32:15 +0700

    secondcommit sudah melengkapi edit text, image, text view, button

--- app/src/main/java/id/sch/smktelkom_mlg/learn/myapplication/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/myapplication/MainActivity.java
@@ -2,12 +2,37 @@ package id.sch.smktelkom_mlg.learn.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    Button bOk;
+    TextView tvHasil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextname);
+        bOk = (Button) findViewById(R.id.buttonok);
+        tvHasil = (TextView) findViewById(R.id.textView3);
+
+
+        bOk.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nama = etNama.getText().toString();
+
+                tvHasil.setText("nama "+nama );
+            }
+        });
+
     }
+
 }
