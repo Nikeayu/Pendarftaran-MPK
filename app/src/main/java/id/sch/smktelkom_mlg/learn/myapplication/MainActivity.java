88bb2abb61a700fb6006deda6369efd567e55e61
ELLEN <nikeayuv15@gmail.com>
2016-10-15 13:13:39 +0700

    thirdcommit sudah melengkapi edit text, image, text view, button,radiobutton, radio group, scroll view

--- app/src/main/java/id/sch/smktelkom_mlg/learn/myapplication/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/myapplication/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,12 +14,16 @@ public class MainActivity extends AppCompatActivity {
     EditText etNama;
     Button bOk;
     TextView tvHasil;
+    RadioButton rblk, rbpr;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rblk = (RadioButton) findViewById(R.id.rblk);
+        rbpr = (RadioButton) findViewById(R.id.rbpr);
 
         etNama = (EditText) findViewById(R.id.editTextname);
         bOk = (Button) findViewById(R.id.buttonok);
@@ -33,6 +39,33 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.buttonok).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
+        });
+    }
+
+    private void doClick() {
+        String hasil = null;
+
+        if(rblk.isChecked())
+        {
+            hasil = rblk.getText().toString();
+        }
+
+        else if(rbpr.isChecked())
+        {
+            hasil = rbpr.getText().toString();
+        }
+        if (hasil==null)
+        {
+            tvHasil.setText("Belum memilih jenis kelamin");
+        }
+        else {
+            tvHasil.setText("Jenis Kelamin :" + hasil);
+        }
     }
 
 }
